c107883cc19f2f4ecf7e29eb5476056a4c01e607
ksazid974-cloud <ksazid974@gmail.com>
2026-04-02 22:20:46 +0530

    Add firebase-config.js setup
    Created firebase-config.js to initialize Firebase app and authentication.
    
    - Imported Firebase SDK (initializeApp, getAuth)
    - Added project Firebase credentials
    - Initialized Firebase app using config
    - Setup authentication module (auth)
    - Exported auth for use in frontend (login/signup)
    
    This file will handle Firebase authentication across the project.

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsCSXhw4939IEx379MDiXl9CTVJjvJLEI",
+  authDomain: "sazio-1.firebaseapp.com",
+  projectId: "sazio-1",
+  storageBucket: "sazio-1.firebasestorage.app",
+  messagingSenderId: "82743199739",
+  appId: "1:82743199739:web:952840299f517d1e47ed5e"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
