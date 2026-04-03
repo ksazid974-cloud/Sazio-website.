230815f61850d5aa93d8059c73a5f0f21c128bab
ksazid974-cloud <ksazid974@gmail.com>
2026-04-03 11:44:01 +0530

    Add Firestore db export
    Updated firebase-config.js to initialize Firestore and export db along with auth for user data storage.

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAsCSXhw4939IEx379MDiXl9CTVJjvJLEI",
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth };
+export { auth, db };
